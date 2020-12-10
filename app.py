baede17d5bc0a66703c32e24d74e6f3015323e6f
Djura Smits <djura.smits@gmail.com>
2020-12-10 11:04:43 +0100

    Created mockup

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-
 import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
 
 app.layout = html.Div(children=[
-    html.H1(children='Manual Task Assistant')
-    ])
-
+    html.H1(children='Manual Task Assistant'),
+    html.Div(children=[
+        html.Blockquote('Task description etc'),
+        html.Div(children=[
+                 'Output 1',
+                 dcc.Input('Output 1')]),
+    html.Button('Confirm')
+]
+)
+]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
