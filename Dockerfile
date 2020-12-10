f031fce21759a03d228f6d899da966f636f16ebd
Djura Smits <djura.smits@gmail.com>
2020-12-10 11:34:49 +0100

    Changed port

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip install -r requirements.txt
 
 CMD python app.py
 
-EXPOSE 8080
+EXPOSE 80
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 HOST = '0.0.0.0'
-PORT = 8080
+PORT = 80
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
 
